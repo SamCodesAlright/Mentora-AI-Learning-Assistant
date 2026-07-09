749cf86dd542ee83076eba0d20b20f0deca175b6
Sameer Shaikh <sameeritis10@gmail.com>
2026-07-09 23:34:21 +0530

    Minute change

--- backend/server.js
+++ backend/server.js
@@ -50,7 +50,7 @@ app.get("/health", (req, res) => {
 app.get("/", (req, res) => {
   res.json({
     success: true,
-    message: "LectureSync Backend is running...",
+    message: "Mentora AI Backend is running...",
   });
 });
 
